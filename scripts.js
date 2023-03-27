33e16e5be518591f67fe2c9fe877aa9c638ad06d
rodrigobaldo <rodrigobaldo@yahoo.com.br>
2023-03-27 19:45:02 -0300

    Criando Função em Button

--- scripts.js
+++ scripts.js
@@ -3,9 +3,20 @@ const button = document.getElementById ('convert-button')
 const dolar = 5.2
 
 const convertValues = () => {
-    const inputReais = document.getElementById('input-real').value
+    const inputReais = document.getElementById('input-real').value;
+    const realValueText = document.getElementById('real-value-text');
+    const currencyValueText = document.getElementById('currency-value');
 
-    console.log(inputReais / dolar)
-}
+
+    realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputReais);
+
+    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    }).format(inputReais / dolar);
+};
 
 button.addEventListener('click', convertValues)
