4fcd97800532da177ad2951f2308da91d5190106
rodrigobaldo <rodrigobaldo@yahoo.com.br>
2023-03-27 21:30:40 -0300

    formartando Changes Dolar/ Euro

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
 const dolar = 5.2
+const euro = 5.9
 
 const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
@@ -14,10 +15,21 @@ const convertValues = () => {
         currency: "BRL",
     }).format(inputReais)
 
-    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-    }).format(inputReais / dolar)
+    if (select.value === 'US$ Dólar Americano') {
+        currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputReais / dolar)
+    }
+
+
+    if (select.value === '€ Euro') {
+        currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputReais / euro)
+    }
+
 }
 
 changeCurrency = () => {
@@ -33,6 +45,8 @@ changeCurrency = () => {
         currencyName.innerHTML = "Euro"
         currencyImg.src = "./assets/Euro.png"
     }
+
+    convertValues()
 }
 
 button.addEventListener('click', convertValues)
